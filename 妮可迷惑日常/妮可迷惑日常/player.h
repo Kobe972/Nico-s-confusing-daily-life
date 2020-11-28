159c9f79b6ce4629f46c301bca026ba88cc4400c
Kobe972 <71507738+Kobe972@users.noreply.github.com>
2020-11-29 00:03:00 +0800

    Add files via upload

--- 妮可迷惑日常/妮可迷惑日常/player.h
+++ 妮可迷惑日常/妮可迷惑日常/player.h
@@ -10,14 +10,15 @@ public:
 	char* GetSkinFile(int SkinID);
 	inline int GetPlayerState();
 	inline int GetPlayerDetail();
-
+	int vx=5, vy=5;//速度
+	int x, y;//在离屏显示区的位置（原点为左下角）。可能设计复杂数学运算，不加前缀
 private:
 	int m_PlayerState;
 	int m_PlayerDetail;				//more accurate state
 	int m_PlayerSkin;				//skin id
 	char* m_skin;					//skin file(bmp)
 	//Size and location was defined in the class CGameEntry
-	RECT m_boarder;					//这里加个注释好吗？
+	RECT m_boarder;					//图像边界（矩形）
 
 private:
 	//建议把绘图相关的东西放在这里
